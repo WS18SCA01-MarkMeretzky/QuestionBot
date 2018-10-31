7972344d166245611a08fd522e6289b77a664019
Mark Meretzky <mark.meretzky@gmail.com>
2018-10-31 17:11:24 -0400

    Initial commit

---
+++ QuestionBot/QuestionAnswerer.swift
@@ -0,0 +1,40 @@
+struct MyQuestionAnswerer {
+    
+    let dislikes: [String] = [
+        "cats",
+        "coconut",
+        "shrimp",
+        "lobster",
+        "shellfish",
+        
+    ];
+    
+    let likes: [String] = [
+        "chinese food",
+        "bicycle",
+        "stanley kubrick",
+        "alfred hitchcock",
+        "astronomy",
+        "cartography"
+    ];
+    
+    
+    func responseTo(question: String) -> String {
+        let lowercaseQuestion: String = question.lowercased();
+
+        for dislike in dislikes {
+            if lowercaseQuestion.range(of: dislike) != nil {
+                return "I detest \(dislike).  You've touched a raw nerve there."
+            }
+        }
+        
+        for like in likes {
+            if lowercaseQuestion.range(of: like) != nil {
+                return "I like \(like)."
+            }
+        }
+
+        return "I have no string opinion.";
+    }
+    
+}
